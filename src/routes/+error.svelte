39d46b1c455de5202ed221247f37a664654e17ef
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-27 12:02:49 +0700

    fix: edit error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,23 +3,20 @@
 	import GradientBackground from '$lib/components/GradientBackground.svelte';
 </script>
 
-<section class="flex flex-col w-full relative text-blue-800 font-google">
-	<GradientBackground />
-	<div class="flex flex-col w-full max-w-7xl mx-auto pt-64 pb-8 py-5 px-6 md:px-10 relative z-10">
+<section class="flex flex-col w-full relative text-white font-sans min-h-screen justify-center">
+	<div class="absolute inset-0 -z-0 overflow-hidden pointer-events-none bg-neutral-50">
+		<img
+			src="/bg.webp"
+			class="block object-cover w-full h-full brightness-70"
+			alt="404 cover page"
+		/>
+	</div>
+	<div class="flex flex-col w-full max-w-7xl mx-auto px-6 md:px-10 relative z-10">
 		<h1 class="text-5xl font-semibold mb-2">Page Status:</h1>
 		<h1 class="text-5xl font-semibold mb-2">{$page.status}</h1>
-		<p class="text-xl">
+		<p class="text-xl mb-4">
 			The page you are looking for is {$page.error?.message}.
 		</p>
-	</div>
-</section>
-<section id="error-details" class="flex flex-col w-full">
-	<div class="flex flex-col w-full max-w-7xl mx-auto py-5 px-6 md:px-10 relative z-10">
-		<a
-			href="/"
-			class="text-5xl font-semibold mb-2 hover:underline hover:underline-offset-4 hover:decoration-4"
-		>
-			>Go to Homepage
-		</a>
+		<a href="/" class="button-white">กลับหน้าแรก</a>
 	</div>
 </section>
